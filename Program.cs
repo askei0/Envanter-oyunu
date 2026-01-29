4ed5447ceeee2a74ba190919eb7df22481c579d8
kXvaNille <kayra9866@gmail.com>
2026-01-29 19:52:18 +0100

    Envanter sistemi eklendi - ilk versiyon

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic; // LİSTE İÇİN BUNU EKLEMEN ŞART!
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 1. Listeyi Oluştur
+        List<string> envanter = new List<string>();
+
+        // 2. Eşyaları Ekle
+        envanter.Add("Kılıç");
+        envanter.Add("Kalkan");
+        envanter.Add("İksir");
+
+        // 3. Sayıyı Yazdır (.Count)
+        Console.WriteLine("Çantada şu an " + envanter.Count + " eşya var.");
+
+        // 4. İksiri Sil
+        envanter.Remove("İksir");
+
+        // 5. Son Durumu Yazdır
+        Console.WriteLine("İksir içildi. Kalan eşya sayısı: " + envanter.Count);
+    }
+}
